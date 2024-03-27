3805d4ab5f19ae1e8b45600f3e66a9c05b0355dc
Flavio Albano <flavio@ingegnerealbano.com>
2024-03-27 20:18:27 +0100

    Create main.py
    The ChatBot

---
+++ main.py
@@ -0,0 +1,58 @@
+# import python-dotenv
+import os
+from dotenv import load_dotenv
+
+# import from huggingface
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+# import regex for clean response
+import re
+
+# import gradio for gui
+import gradio as gr
+
+
+# take environment variables from .env file
+load_dotenv()
+token = os.getenv('TOKEN')
+
+# set some stuffs
+model_id = "google/gemma-2b-it"
+dtype = torch.bfloat16
+history = list()
+
+# start with chat
+def gemma_chat(message, history):
+    tokenizer = AutoTokenizer.from_pretrained(model_id, token=token)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        token=token,
+        hidden_activation="gelu_pytorch_tanh",
+        device_map="cuda",
+        torch_dtype=dtype,
+    )
+
+    chat = [
+        { "role": "user", "content": message },
+    ]
+
+    prompt = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+    
+    inputs = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+    outputs = model.generate(input_ids=inputs.to(model.device), max_new_tokens=2048)
+    
+    response = tokenizer.decode(outputs[0])
+    
+    
+    # clean the response
+    response_cleaned = re.split("model", response)
+    
+    # update the history
+    history.append([message, response_cleaned[1]])    
+
+    # return the response
+    return response_cleaned[1]
+
+
+gr.ChatInterface(gemma_chat).launch()
